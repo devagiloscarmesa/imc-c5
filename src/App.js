3e5fc7e5385e86b811bb31b7d427d3bb55585b1b
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-12-17 09:36:49 -0500

    Cambios con react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Calculadora from './components/section1/CalculatorApp'
+import ViewAppImc from './components/section2/ViewAppImc'
 import UsuarioContext from './context/UsuarioContext'
 function App() {
   return (
     <div className="App">
-      <UsuarioContext.Provider value = {{
-           id : 10,
-           nombres : "Oscar",
-           apellidos : "Mesa",
-           edad : 23,
-           correo : 'oscar.mesa@gmail.com'
-      }}>
-        <Calculadora />
-      </UsuarioContext.Provider>
+      <BrowserRouter>
+        <UsuarioContext.Provider value={{
+          id: 10,
+          nombres: "Oscar",
+          apellidos: "Mesa",
+          edad: 23,
+          correo: 'oscar.mesa@gmail.com'
+        }}>
+
+
+          <Switch>
+            <Route path="/" exact />
+            <Route path="/imc-toma" exact render = {(props) => {return <Calculadora {...props} nombre = "oscar"/>}} />
+            <Route path="/usuario/iniciar-sesion" exact />
+            <Route path="/imc-paciente" exact />
+            <Route path="/imc-estadistica" exact children ={ () => <ViewAppImc/> } />
+          </Switch>
+        </UsuarioContext.Provider>
+      </BrowserRouter>
     </div>
   );
 }
